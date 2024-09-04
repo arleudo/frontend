47012a32842e583f11724a26931d2dd9c7b3d1d0
arleudo <antonioarleudo@gmail.com>
2024-09-04 20:43:20 -0300

    third commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui";
+import { useNavigate } from 'react-router-dom';
+
 export function Home() {
+    const navigate = useNavigate();
+
     return (
-        <>
-            Home
-        </>
+        <div className="flex flex-row justify-center gap-10 mt-10">
+            <Button onClick={() => {
+                navigate('/pressions');
+            }} >Controle de Pressão Arterial</Button>
+            <Button onClick={() => {
+                navigate('/users');
+            }}>Controle de Usuários</Button>
+        </div>
     );
 }
